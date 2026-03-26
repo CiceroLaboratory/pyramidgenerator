a7dc16e92d87a29d3b8af4d98b305e887acd8b26
cicero <genivaldocicerobatistajunior@gmail.com>
2026-03-26 01:43:28 -0300

    part 1

---
+++ index.js
@@ -0,0 +1,206 @@
+//Step 1
+console.log("Let's build a pyramid using JavaScript basics:");
+console.log("    #    ");
+console.log("   ###   ");
+console.log("  #####  ");
+console.log(" ####### ");
+console.log("#########");
+
+//Step 2
+let character;
+
+//Step 3
+character = "Hello";
+
+//Step 4
+character = 'Hello';
+
+//Step 5
+character = 'Hello';
+console.log(character)
+
+//Step 6
+character = "World"
+console.log(character);
+
+//Step 7
+character = 'Hello';
+console.log(character);
+character = "World";
+console.log(character)
+
+//Step 8
+let secondCharacter;
+
+//Step 9
+console.log(secondCharacter)
+
+//Step 10
+secondCharacter = "Test"
+
+//Step 11
+secondCharacter = character;
+
+//Step 12
+character = 'Hello';
+
+//Step 13
+let profession = "teacher";
+let age;
+console.log(age)
+console.log(profession)
+
+//Step 14 
+character = "Hello";
+
+//Step 15 
+let count = 8;
+
+//Step 16 
+character = 'Hello';
+count = 8;
+console.log(count + 1)
+
+//Step 17
+// remove conole.log
+
+//Step 18
+let rows = []
+
+//Step 19
+rows = ["Naomi", "Quincy", "CamperChan"];
+
+//Step 20
+console.log(rows[0])
+
+//Step 21
+rows[2] = 10
+console.log(rows)
+
+//Step 22
+rows[rows.length - 1]
+
+//Step 23
+// Just remove 
+
+//Step 24
+cities[cities.length - 1] = "Mexico City"
+console.log(cities);
+
+// Step 25
+//Remove all of your code from the previous step.
+
+// Step 26
+rows.push("freeCodeCamp")
+
+// Step 27
+let popped = rows.pop()
+console.log(popped);
+
+// Step 28
+let pushed = rows.push("freeCodeCamp");
+console.log(pushed);
+
+// Step 29
+//remove some things
+
+// Step 30
+character = "Hello";
+count = 8;
+let rows = [];
+
+// Step 31
+// Just change let for const
+
+// Step 32
+character = "#";
+
+// Step 33
+for ('iterator'; "condition"; "iteration") { }
+
+// Step 34
+for (let i = 0; "condition"; "iteration") { }
+
+// Step 35
+for (let i = 0; i < count; "iteration") { }
+
+// Step 36
+for (let i = 0; i < count; i = i + 1) { }
+
+// Step 37
+console.log(i)
+
+// Step 38
+rows.push(i);
+
+// Step 39
+let result = "";
+
+// Step 40
+console.log(result)
+
+// Step 41
+for (const row of rows){}
+
+// Step 42
+for (const row of rows) {
+  result = result + row
+}
+
+// Step 43
+for (const row of rows) {
+  result = result + row + "\n";
+}
+
+// Step 44
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character);
+}
+
+// Step 45
+for (let i = 0; i < count; i = i + 1) {
+  
+  rows.push(character.repeat(i));
+}
+
+// Step 46
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i+1))
+}
+
+// Step 47
+function padRow(){}
+
+// Step 48
+padRow()
+
+// Step 49
+const call = padRow();
+
+// Step 50
+console.log(call);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
